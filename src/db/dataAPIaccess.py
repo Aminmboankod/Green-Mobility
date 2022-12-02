04177d07561198e19403e31c7d5b62d088c3ea62
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2022-12-02 13:16:54 +0100

    feat(snippet):Added snippet for connecting to Mongodb

---
+++ src/db/dataAPIaccess.py
@@ -0,0 +1,22 @@
+import json
+import requests
+
+url = "https://data.mongodb-api.com/app/data-ijmfb/endpoint/data/v1/action/find"
+
+payload = json.dumps(
+    {
+    "collection": "bikes",
+    "database": "GreenMobility",
+    "dataSource": "Cluster0",
+    "filter": {
+
+    }})
+
+headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Request-Headers': '*',
+    'api-key': '',
+    'Accept': 'application/json'
+    }
+Querie = requests.post(url, headers=headers, data=payload)
+Querie = Querie.text
