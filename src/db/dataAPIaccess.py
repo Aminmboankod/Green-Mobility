7784758777b61a07f888c4f6a5ff04d1e241f016
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2022-12-02 23:59:20 +0100

    feat(createJSON): Implement condicionals for errors and added json document

--- src/db/dataAPIaccess.py
+++ src/db/dataAPIaccess.py
@@ -15,12 +15,23 @@ payload = json.dumps(
 headers = {
     'Content-Type': 'application/json',
     'Access-Control-Request-Headers': '*',
-    'api-key': '',
+    'api-key': 'Hc28jQ1d3AefiXjJ0Z8xWdaL8OWc93aTTm8L2RuuhCh0ZADccNe4Hr6zEOerBM5o',
     'Accept': 'application/json'
     }
-Query = requests.post(url, headers=headers, data=payload)
-# aqui debemos controlar la conexión e implementar control de errores (try, except y finally)
-Query = Query.text(json.loads(Query["documents"]))
 
-createJson = open("GreenMobility.json", "w+", encoding="UTF-8")
-createJson.write(Query)
+
+
+
+
+query = requests.post(url, headers=headers, data=payload)
+status = query.status_code
+if status == 200:
+    print("successful connection!")
+    query = query.text
+    query = json.loads(query)
+    query = query["documents"]
+    createJson = open("GreenMobility.json", "w+", encoding="UTF-8")
+    
+else:
+    if status == 401:
+        print("unauthorized user")
