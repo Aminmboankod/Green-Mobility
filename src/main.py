af7be6f89580b9c5e9150dc83d22049665b1c8dd
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2022-12-04 17:21:57 +0100

    feat(createListHTML): function that creates the content

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
 ## EN ESTE FICHERO INCLUIREMOS LAS FUNCIONES QUE YA ESTÉN TERMINADAS PARA EL ORDEN DE EJECUCIÓN ##
 
 
-#if __name__=="__main__":
+
+
+from db.dataAPIaccess import dataBikes     # importamos módulo que consulta extrae datos de mongodb
+
+if __name__=="__main__":
+
+    dictionaryBikes = dataBikes()          # usamos datos del módulo de conexión a mongodb y los añadimos a una variable que usaremos como parámetro.
