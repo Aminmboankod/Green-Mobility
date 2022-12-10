58bd68c7616a751e8d5d03924dc5a087b53dce49
Antonio Maroto <antoniomarotoblasco@gmail.com>
2022-12-10 12:45:25 +0100

    enchacement(test): Creating new tests

--- src/db/crud/allBikes.py
+++ src/db/crud/allBikes.py
@@ -1,9 +1,10 @@
-
 import json
 import os
 
 KEY = os.environ["APIKEY"]
 URL = os.environ["URL"]
+
+###### Función que acumula en varaible TODAS las bicicletas de la base de datos #####
 
 def dataBikes():
     from db.mainCRUD import CRUD
